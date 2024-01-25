42bc660799613b73812cde710b268a1974de454e
Tetsu-is <hap30305@kwansei.ac.jp>
2024-01-26 00:28:06 +0900

    add crud api

--- main.go
+++ main.go
@@ -24,10 +24,55 @@ func main() {
 	db.AutoMigrate(&Student{})
 
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Pong",
 		})
 	})
+
+	//create student
+	r.POST("/student", func(c *gin.Context) {
+		var student Student
+		c.BindJSON(&student)
+		db.Create(&student)
+		c.JSON(200, student)
+	})
+
+	//read all students
+	r.GET("/students", func(c *gin.Context) {
+		var students []Student
+		if err := db.Find(&students).Error; err != nil {
+			c.AbortWithStatus(404)
+		} else {
+			c.JSON(200, students)
+		}
+	})
+
+	//read student by student_id
+	r.GET("/student/:student_id", func(c *gin.Context) {
+		var student Student
+		id := c.Params.ByName("student_id")
+		if err := db.Where("student_id = ?", id).First(&student).Error; err != nil {
+			c.AbortWithStatus(404)
+		} else {
+			c.JSON(200, student)
+		}
+	})
+
+	//update student by id
+	r.PUT("/student/:student_id", func(c *gin.Context) {
+		var student Student
+		id := c.Params.ByName("student_id")
+		if err := db.Where("student_id = ?", id).First(&student).Error; err != nil {
+			c.AbortWithStatus(404)
+		}
+		c.BindJSON(&student)
+		db.Save(&student)
+		c.JSON(200, student)
+	})
+
+	//delete student by id
+
 	r.Run()
 }
