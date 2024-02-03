aceb7415e9e5e2d2330b42315b00a2cc4b70d2e1
Tetsu-is <hap30305@kwansei.ac.jp>
2024-02-03 16:35:02 +0900

    use .env for connecting db

--- api/database/database.go
+++ api/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,7 +10,13 @@ import (
 var db *gorm.DB
 
 func Init() {
-	dsn := "root:password@tcp(mysql:3306)/my_db?charset=utf8mb4&parseTime=True&loc=Local"
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlPort := os.Getenv("MYSQL_PORT")
+	mysqlDB := os.Getenv("MYSQL_DATABASE")
+	dsn := mysqlUser + ":" + mysqlPassword + "@tcp(" + mysqlHost + ":" + mysqlPort + ")/" + mysqlDB + "?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:password@tcp(mysql:3306)/my_db?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
