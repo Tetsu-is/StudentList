5cd06bab24692aa9edbba0a03eaf17cb60e0380f
Tetsu-is <hap30305@kwansei.ac.jp>
2024-02-03 16:48:07 +0900

    setting for cors

--- api/controller/router.go
+++ api/controller/router.go
@@ -1,9 +1,16 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"student/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(middleware.Cors())
+
 	r.POST("/student", CreateStudent)
 	r.GET("/student", ReadAllStudents)
 	r.GET("/student/:student_id", ReadStudentByID)
